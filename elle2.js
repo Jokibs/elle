77eadbec0e7c78dcf95f9804e23e46df63a9c48d
Jokibs <122084307+Jokibs@users.noreply.github.com>
2023-02-21 12:49:43 +0300

    Add files via upload

---
+++ elle2.js
@@ -0,0 +1,21 @@
+var i=0;
+var images=[];
+var time=1000;
+//images list
+images[0]='pic1.jpg';
+images[1]='pic11.jpg';
+images[2]='pic14.jpg';
+images[3]='new.jpg';
+
+
+//change image
+function changeimg(){
+    document.slide.src=images[i];
+    if(i< images.length-1){
+    i++;
+        
+    }else{i=0;}
+    
+    setTimeout("changeimg()",time);
+}
+window.onload=changeimg;
