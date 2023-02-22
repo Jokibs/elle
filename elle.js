9ad8f876f61d6129b55cb9d047fde6658341decc
Jokibs <122084307+Jokibs@users.noreply.github.com>
2023-02-22 13:06:28 +0300

    Update elle.js

--- elle.js
+++ elle.js
@@ -2,9 +2,9 @@ var i=0;
 var images=[];
 var time=1000;
 //images list
-images[0]='pic6.jpg';
-images[1]='pic7.jpg';
-images[2]='pic8.jpg';
+images[0]='pic15.webp';
+images[1]='pic17.webp';
+images[2]='pic14.webp';
 images[3]='pic9.jpg';
 images[4]='pic10.jpg';
 images[5]='pic5.jpg';
