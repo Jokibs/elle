5450ce4d66e157d82ba51aa294651e3fdb8bff55
Jokibs <122084307+Jokibs@users.noreply.github.com>
2023-02-25 22:52:26 +0300

    Update elle.js

--- elle.js
+++ elle.js
@@ -24,26 +24,38 @@ function changeimg(){
 }
 window.onload=changeimg;
 var i=0;
-var images=[];
-var time=2000;
+var var i1 = 0;
+var images1 = ['pic1.jpg', 'pic2.jpg', 'pic3.jpg', 'pic4.jpg'];
+var time1 = 2000;
 
-//images list
-images[0]='pic4.jpg';
-images[1]='pic3.jpg';
-images[2]='pic2.jpg';
-images[3]='pic1.jpg';
-
-//change image
-function changeimg2(){
-    document.two.src=images[i];
-    if(i< images.length-1){
-    i++;
-        
-    }else{i=0;}
-    
-    setTimeout("changeimg2()",time);
+function changeimg1() {
+    document.slide.src = images1[i1];
+    if (i1 < images1.length - 1) {
+        i1++;
+    } else {
+        i1 = 0;
+    }
+    setTimeout(changeimg1, time1);
 }
+
+    
+         
+var i2 = 0;
+var images2 = ['pic1.jpg', 'pic11.jpg', 'pic14.jpg'];
+var time2 = 2000;
+
+function changeimg2() {
+    document.two.src = images2[i2];
+    if (i2 < images2.length - 1) {
+        i2++;
+    } else {
+        i2 = 0;
+    }
+    setTimeout(changeimg2, time2);
+}
+
 window.onload = function() {
-  changeimg1();
-  changeimg2();
+    changeimg1();
+    changeimg2();
 };
+
